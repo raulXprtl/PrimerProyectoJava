e2e35cc0003a1451d76c15dc52d96714cbc3961c
raulXprtl <raul.ceron@xpertal.com>
2022-01-19 15:29:34 -0600

    Primer commit con ejercicios 1 y 2 en .\src\bootcamp\Main.java

---
+++ src/bootcamp/Main.java
@@ -0,0 +1,23 @@
+package bootcamp;
+
+public class Main {
+    public static void Ejercicio1() {
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean estudia = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+    }
+
+    public static void Ejercicio2() {
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;
+        int cantidad_de_hijos;
+    }
+
+}
